524fe4e5bd2304cebb95f3222befb68bea2ee1df
McKayla Lankau <lankaukk@yahoo.com>
2020-07-23 23:41:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,10 +35,10 @@ def won?(board)
   if WIN_COMBINATIONS == nil 
     return false
     
-  elsif board[top_row_win[0]] == "X" && board[WIN_COMBINATIONS[0][1]] == "X" && board[WIN_COMBINATIONS[0][2]] == "X"
+  elsif board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
     "X won in the top row"
     return WIN_COMBINATIONS[0] 
-  elsif board[WIN_COMBINATIONS[0][0]] == "O" && board[WIN_COMBINATIONS[0][1]] == "O" && board[WIN_COMBINATIONS[0][2]] == "O"
+  elsif board[middle_row_win[0]] == "O" && board[WIN_COMBINATIONS[0][1]] == "O" && board[WIN_COMBINATIONS[0][2]] == "O"
     "O won in the top row"
     return WIN_COMBINATIONS[0] 
     
