342c6fe5738824cd5c6472ba0fc65334377487aa
McKayla Lankau <lankaukk@yahoo.com>
2020-07-23 23:31:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,130 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [top_row_win = 
+    [0,1,2]
+  ],
+  [middle_row_win = 
+    [3,4,5]
+  ],
+  [bottom_row_win = 
+    [6,7,8]
+  ], 
+  [left_column_win =
+    [0,3,6]
+  ],
+  [middle_column_win =
+    [1,4,7]
+  ], 
+  [right_column_win =
+    [2,5,8]
+  ],
+  [left_diagonal_win =
+    [0,4,8]
+  ],
+  [right_diagonal_win =
+    [2,4,6]
+  ]
+]
+
+def won?(board)
+  if WIN_COMBINATIONS == nil 
+    return false
+    
+  elsif board[top_row_win[0]] == "X" && board[WIN_COMBINATIONS[0][1]] == "X" && board[WIN_COMBINATIONS[0][2]] == "X"
+    "X won in the top row"
+    return WIN_COMBINATIONS[0] 
+  elsif board[WIN_COMBINATIONS[0][0]] == "O" && board[WIN_COMBINATIONS[0][1]] == "O" && board[WIN_COMBINATIONS[0][2]] == "O"
+    "O won in the top row"
+    return WIN_COMBINATIONS[0] 
+    
+  elsif board[WIN_COMBINATIONS[1][0]] == "X" && board[WIN_COMBINATIONS[1][1]] == "X" && board[WIN_COMBINATIONS[1][2]] == "X"
+    "X won in the middle row"
+    return WIN_COMBINATIONS[1]
+  elsif board[WIN_COMBINATIONS[1][0]] == "O" && board[WIN_COMBINATIONS[1][1]] == "O" && board[WIN_COMBINATIONS[1][2]] == "O"
+    "O won in the middle row"
+    return WIN_COMBINATIONS[1]
+    
+  elsif board[WIN_COMBINATIONS[2][0]] == "X" && board[WIN_COMBINATIONS[2][1]] == "X" && board[WIN_COMBINATIONS[2][2]] == "X"
+    "X won in the bottom row"
+    return WIN_COMBINATIONS[2] 
+  elsif board[WIN_COMBINATIONS[2][0]] == "O" && board[WIN_COMBINATIONS[2][1]] == "O" && board[WIN_COMBINATIONS[2][2]] == "O"
+    "O won in the bottom row"
+    return WIN_COMBINATIONS[2] 
+    
+  elsif board[WIN_COMBINATIONS[3][0]] == "O" && board[WIN_COMBINATIONS[3][1]] == "O" && board[WIN_COMBINATIONS[3][2]] == "O"
+    "O won in the left column"
+    return WIN_COMBINATIONS[3] 
+  elsif board[WIN_COMBINATIONS[3][0]] == "X" && board[WIN_COMBINATIONS[3][1]] == "X" && board[WIN_COMBINATIONS[3][2]] == "X"
+    "X won in the left column"
+    return WIN_COMBINATIONS[3]
+    
+  elsif board[WIN_COMBINATIONS[4][0]] == "O" && board[WIN_COMBINATIONS[4][1]] == "O" && board[WIN_COMBINATIONS[4][2]] == "O"
+    "O won in the middle column"
+    return WIN_COMBINATIONS[4]
+  elsif board[WIN_COMBINATIONS[4][0]] == "X" && board[WIN_COMBINATIONS[4][1]] == "X" && board[WIN_COMBINATIONS[4][2]] == "X"
+    "X won in the middle column"
+    return WIN_COMBINATIONS[4]
+    
+  elsif board[WIN_COMBINATIONS[5][0]] == "O" && board[WIN_COMBINATIONS[5][1]] == "O" && board[WIN_COMBINATIONS[5][2]] == "O"
+    "O won in the right column"
+    return WIN_COMBINATIONS[5]
+  elsif board[WIN_COMBINATIONS[5][0]] == "X" && board[WIN_COMBINATIONS[5][1]] == "X" && board[WIN_COMBINATIONS[5][2]] == "X"
+    "X won in the right column"
+    return WIN_COMBINATIONS[5]
+    
+  elsif board[WIN_COMBINATIONS[6][0]] == "O" && board[WIN_COMBINATIONS[6][1]] == "O" && board[WIN_COMBINATIONS[6][2]] == "O"
+    "O won in the left diagonal"
+    return WIN_COMBINATIONS[6]
+  elsif board[WIN_COMBINATIONS[6][0]] == "X" && board[WIN_COMBINATIONS[6][1]] == "X" && board[WIN_COMBINATIONS[6][2]] == "X"
+    "X won in the left diagonal"
+    return WIN_COMBINATIONS[6]
+    
+  elsif board[WIN_COMBINATIONS[7][0]] == "O" && board[WIN_COMBINATIONS[7][1]] == "O" && board[WIN_COMBINATIONS[7][2]] == "O"
+    "O won in the right diagonal"
+    return WIN_COMBINATIONS[7]
+  elsif board[WIN_COMBINATIONS[7][0]] == "X" && board[WIN_COMBINATIONS[7][1]] == "X" && board[WIN_COMBINATIONS[7][2]] == "X"
+    "X won in the right diagonal"
+    return WIN_COMBINATIONS[7]
+  
+  end
+end
+
+def full?(board)
+  if board.all?{|i| i != " "}
+    return true 
+  elsif board.any?{|i| i == " "}
+    return false
+  end
+end
+
+def draw?(board)
+  if won?(board) == nil &&
+    full?(board) == true
+    return true
+  elsif 
+    false 
+  end
+end
+
+def over?(board)
+  if won?(board) == true &&
+    full?(board) == true
+    return true
+  elsif won?(board) == true &&
+    full?(board) == false
+    return true
+  elsif draw?(board) == true
+    return true
+  elsif
+    false
+  end
+end
+
+def winner(board)
+  if draw?(board) == true
+    return nil
+  end
+end
+
